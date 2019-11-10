3b5abecbc65af428465af24a742f5c76735ad2da
tarandeep2210 <tarandeep2210@gmail.com>
2019-11-10 17:43:24 +0530

    Find Distance Added

--- index.js
+++ index.js
@@ -34,6 +34,30 @@ app.get('/students',async (req,res)=>{
 
 app.use('/auth' , auth.router);
 
+console.log(findDistance(12.939888,77.627011,12.939888,77.626975));
+
+function findDistance(lat1, lon1, lat2, lon2) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1.609344 * 1000;
+	
+		return dist;
+	}
+}
+
 
 app.listen(3000, () =>{
     console.log('Server is running at port 3000');
